70b69b3e0693f3a72d523cdb10b4a4935d7f6fce
sandym <91996681+sandyml@users.noreply.github.com>
2023-02-09 13:29:00 -0500

    Updated

--- app/controllers/locates_controller.rb
+++ app/controllers/locates_controller.rb
@@ -9,14 +9,12 @@ class LocatesController < ApplicationController
     if location.save
     render json: location, status: :created
     else
-    # unprocessable_entity_errors_response
     render json: { errors: location.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def location_params 
     params.require(:locate).permit(:trail_name, :city, :state, :image_url, :difficulty, :length, :elevation_gain, :route_type)
-    # params.permit(:trail_name, :city, :state, :image_url, :difficulty, :length, :elevation_gain, :route_type)
   end
 
  # def unprocessable_entity_errors_response
